9f567b5ec282f91533152b243056ff7efb164fd6
Jaslyn <jaslyn@siliconjungles.com>
2019-12-19 16:56:10 +0800

    Remove javascript files

--- src/increment.ts
+++ src/increment.ts
@@ -3,7 +3,7 @@ import Project from '../lib/Project'
 import Tools from '../lib/Tools'
 
 // TODO: Split increment into build_number & version_name
-export function increment() {
+export function increment(): void {
 	let xcode_path: string = Arg.v_null('xcode_path') || Project.find_xcode_proj()
 	let gradle_path: string = Arg.v_null('gradle_path') || 'android/app/build.gradle'
 	let build_src: string | undefined = Arg.v_enum_null('build_src', ['git'])
