66fbfd30d4ba2883462c8d7f019ff9066b0804d6
Jaslyn <jaslyn@siliconjungles.com>
2019-12-20 13:24:31 +0800

    Prettified

--- src/set_build_num.ts
+++ src/set_build_num.ts
@@ -3,17 +3,17 @@ import Project from './lib/Project'
 import Tools from './lib/Tools'
 
 export function set_build_num(): void {
-	let xcode_path: string 				= 	Arg.v_null('xcode_path') || Project.find_xcode_proj()
-	let gradle_path: string 			= 	Arg.v_null('gradle_path') || 'android/app/build.gradle'
-	let build_src: string | undefined 	= 	Arg.v_enum_null('src', ['git'])
-	let platform: string 				= 	Arg.v_enum_null('platform', ['android', 'ios', 'both']) || 'both'
-	let base_num: number | undefined	= 	Arg.v_number_null('base_num')
+	let xcode_path: string 				= Arg.v_null('xcode_path') || Project.find_xcode_proj()
+	let gradle_path: string 			= Arg.v_null('gradle_path') || 'android/app/build.gradle'
+	let build_src: string | undefined 	= Arg.v_enum_null('src', ['git', 'time'])
+	let platform: string 				= Arg.v_enum_null('platform', ['android', 'ios', 'both']) || 'both'
+	let base_num: number | undefined	= Arg.v_number_null('base_num')
 
-	let build_number: number			= 	(build_src === 'git') ?
-		Tools.version_from_git('develop')	: 	Arg.v_number('num')
-	if (base_num) {
-		build_number = build_number + base_num
-	}
+	let build_number: number
+	if ('git' === build_src)  		build_number = Tools.version_from_git('develop')
+	else if ('time' === build_src) 	build_number = Tools.version_from_epoch()
+	else 							build_number = Arg.v_number('num')
+	if (base_num) 					build_number += base_num
 
 	if (platform !== 'ios') {
 		Project.write_gradle(gradle_path, build_number, undefined)
--- src/set_ver_name.ts
+++ src/set_ver_name.ts
@@ -2,17 +2,16 @@ import Arg from './lib/Arg'
 import Project from './lib/Project'
 import Tools from './lib/Tools'
 
-// TODO: Split increment into build_number & version_name
 export function set_ver_name(): void {
-	let xcode_path: string 					= 	Arg.v_null('xcode_path') || Project.find_xcode_proj()
-	let gradle_path: string 				= 	Arg.v_null('gradle_path') || 'android/app/build.gradle'
-	let platform: string 					= 	Arg.v_enum_null('platform', ['android', 'ios', 'both']) || 'both'
+	let xcode_path: string 			= 	Arg.v_null('xcode_path') || Project.find_xcode_proj()
+	let gradle_path: string 		= 	Arg.v_null('gradle_path') || 'android/app/build.gradle'
+	let platform: string 			= 	Arg.v_enum_null('platform', ['android', 'ios', 'both']) || 'both'
 	let src: string | undefined 	= 	Arg.v_enum_null('src', ['package.json'])
 	let suffix: string | undefined 	= 	Arg.v_null('suffix')
 
-	let ver_name: string | undefined 	= 	(src === 'package.json') ?
-		Tools.version_from_package()		: 	Arg.v('name')
-	ver_name = suffix ? `${ver_name}-${suffix}` : ver_name
+	let ver_name: string | undefined = (src === 'package.json') ?
+		Tools.version_from_package() : Arg.v('name')
+	if (suffix) ver_name = `${ver_name}-${suffix}`
 
 	if (platform !== 'ios') {
 		Project.write_gradle(gradle_path, undefined, ver_name)
